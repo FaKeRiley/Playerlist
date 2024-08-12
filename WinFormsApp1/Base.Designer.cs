8204b8784790835847796b8d6a47de61cd6a7c8b
FaKeRiley <FaKeRiley@Desktop-SWKAP>
2024-08-12 20:31:23 +0200

    Projektdateien hinzufügen.

---
+++ WinFormsApp1/Base.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WinFormsApp1
+{
+    partial class Base
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Base));
+            panel1 = new Panel();
+            button3 = new Button();
+            panel2 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            panel3 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaptionText;
+            panel1.Controls.Add(button3);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(801, 63);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            panel1.MouseDown += MouseDown1;
+            panel1.MouseMove += MouseMove1;
+            panel1.MouseUp += MouseUp1;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BackgroundImageLayout = ImageLayout.None;
+            button3.Cursor = Cursors.Hand;
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(768, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(20, 20);
+            button3.TabIndex = 0;
+            button3.Text = "x";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaptionText;
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(0, 51);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(114, 400);
+            panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(22, 208);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "WorldList";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(22, 129);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Playerlist";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.BackgroundImageLayout = ImageLayout.Stretch;
+            panel3.Location = new Point(114, 54);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(687, 397);
+            panel3.TabIndex = 2;
+            panel3.Paint += panel3_Paint;
+            // 
+            // Base
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Base";
+            Text = "Base";
+            Load += Base_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Button button2;
+        private Panel panel3;
+        private Button button3;
+    }
+}
---
+++ WinFormsApp1/Base.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Base : Form
+    {
+        private Playerlist Playerlist1;
+        private Worldlist Worldlist1;
+
+        private bool isDragging = false;
+        private Point offset;
+
+        public Base()
+        {
+            InitializeComponent();
+            // Event-Handler für das Panel hinzufügen
+            this.panel1.MouseDown += new MouseEventHandler(MouseDown1);
+            this.panel1.MouseMove += new MouseEventHandler(MouseMove1);
+            this.panel1.MouseUp += new MouseEventHandler(MouseUp1);
+        }
+
+        private void Base_Load(object sender, EventArgs e)
+        {
+            // Falls weitere Initialisierungen erforderlich sind
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Playerlist1 == null || Playerlist1.IsDisposed)
+            {
+                // Öffne Playerlist
+                Playerlist1 = new Playerlist();
+                Playerlist1.Show();
+            }
+            else
+            {
+                // Schließe Playerlist
+                Playerlist1.Close();
+                Playerlist1 = null; // Setze die Referenz zurück
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Worldlist1 == null || Worldlist1.IsDisposed)
+            {
+                // Öffne Worldlist
+                Worldlist1 = new Worldlist();
+                Worldlist1.Show();
+            }
+            else
+            {
+                // Schließe Worldlist
+                Worldlist1.Close();
+                Worldlist1 = null; // Setze die Referenz zurück
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            // Falls weitere Zeichenoperationen erforderlich sind
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Falls weitere Zeichenoperationen erforderlich sind
+        }
+
+        private void MouseDown1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                // Berechne den Offset relativ zur oberen linken Ecke des Panels
+                offset = e.Location;
+            }
+        }
+
+        private void MouseMove1(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                // Berechne die aktuelle Bildschirmposition des Mauszeigers
+                Point mouseScreenPos = Control.MousePosition;
+
+                // Berechne die neue Position der Form
+                this.Location = new Point(
+                    mouseScreenPos.X - offset.X,
+                    mouseScreenPos.Y - offset.Y
+                );
+            }
+        }
+
+        private void MouseUp1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
